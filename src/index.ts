db8b45be3dc61b5f0eb91b4395002b08e9fbc3c2
Daniel Sales Álvarez <dsa21may@gmail.com>
2021-03-29 13:23:45 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 export {
- 
-};
+  destructDate,
+  destructDateUTC,
+} from "./DestructDate";
+export {
+  default as Month,
+} from "./Month";
+export {
+  getTimestamp,
+  getTimestampNow,
+} from "./Timestamp";
+export {
+  daysBetween,
+  utcToLocal,
+} from "./TimeUtils";
+export {
+  default as WeekDay,
+} from "./WeekDay";
