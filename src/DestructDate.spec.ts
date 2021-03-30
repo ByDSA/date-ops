89aa44d6c4cb8df1093aa433e45b234571a2ed88
Daniel Sales Álvarez <dsa21may@gmail.com>
2021-03-30 10:05:28 +0200

    Fix destructDate with locale invariable. Add destructDateUTC

--- src/DestructDate.spec.ts
+++ src/DestructDate.spec.ts
@@ -1,4 +1,4 @@
-import { destructJSDate } from "./DestructDate";
+import { destructJSDate, destructJSDateUTC } from "./DestructDate";
 import MonthJS from "./MonthJS";
 import WeekDayJS from "./WeekDayJS";
 
@@ -25,6 +25,44 @@ describe("normal", () => {
     expect(month).toBe(MonthJS.MAY);
     expect(day).toBe(23);
     expect(hours).toBe(8);
+    expect(minutes).toBe(34);
+    expect(seconds).toBe(0);
+    expect(dweek).toBe(WeekDayJS.SUNDAY);
+  } );
+
+  it("no param", () => {
+    const { year, month, day, hours, minutes, seconds, dweek } = destructJSDate();
+
+    expect(year).toBeDefined();
+    expect(month).toBeDefined();
+    expect(day).toBeDefined();
+    expect(hours).toBeDefined();
+    expect(minutes).toBeDefined();
+    expect(seconds).toBeDefined();
+    expect(dweek).toBeDefined();
+  } );
+} );
+
+describe("utc", () => {
+  it("testDate", () => {
+    const { year, month, day, hours, minutes, seconds, dweek } = destructJSDateUTC(testDate);
+
+    expect(year).toBe(1993);
+    expect(month).toBe(MonthJS.MAY);
+    expect(day).toBe(21);
+    expect(hours).toBe(6);
+    expect(minutes).toBe(34);
+    expect(seconds).toBe(0);
+    expect(dweek).toBe(WeekDayJS.FRIDAY);
+  } );
+
+  it("testDate2", () => {
+    const { year, month, day, hours, minutes, seconds, dweek } = destructJSDateUTC(testDate2);
+
+    expect(year).toBe(1993);
+    expect(month).toBe(MonthJS.MAY);
+    expect(day).toBe(23);
+    expect(hours).toBe(6);
     expect(minutes).toBe(34);
     expect(seconds).toBe(0);
     expect(dweek).toBe(WeekDayJS.SUNDAY);
--- src/DestructDate.ts
+++ src/DestructDate.ts
@@ -1,12 +1,34 @@
-/* eslint-disable import/prefer-default-export */
+export function destructJSDateUTC(date: Date = new Date()) {
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth();
+  const year = date.getUTCFullYear();
+  const dweek = date.getUTCDay();
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds();
+
+  return {
+    day,
+    month,
+    year,
+    hours,
+    minutes,
+    seconds,
+    dweek,
+  };
+}
+
 export function destructJSDate(date: Date = new Date()) {
-  const day = date.getDate();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const dweek = date.getDay();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
+  const dateTime = date.getTime();
+  const offsetMillis = -date.getTimezoneOffset() * 60 * 1000;
+  const dateCopy = new Date(dateTime + offsetMillis);
+  const day = dateCopy.getDate();
+  const month = dateCopy.getMonth();
+  const year = dateCopy.getFullYear();
+  const dweek = dateCopy.getDay();
+  const hours = dateCopy.getHours();
+  const minutes = dateCopy.getMinutes();
+  const seconds = dateCopy.getSeconds();
 
   return {
     day,
